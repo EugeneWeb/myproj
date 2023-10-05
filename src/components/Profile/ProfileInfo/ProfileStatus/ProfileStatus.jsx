a48d80668c8c59b3b61a4e7b1c9a9b31d9b5caad
EugeneWeb <evgenijbobrov19@gmail.com>
2023-10-05 18:39:06 +0300

    Почему раньше нельзя было использовать методы жизненного цикла, стэйты и т.д в функциональных компонентах, как это происходит сейчас, что возвращает useState, как работает useState, useEffect, имитация componentDidMount с useEffect(стоит ли), где нельзя использовать хуки

--- src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.jsx
@@ -1,48 +1,74 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import s from './ProfileStatus.module.css'
 
 
-class ProfileStatus extends Component {
-    state = {
-        editMode: false,
-        status: this.props.status
-    }
+// class ProfileStatus extends Component {
+//     state = {
+//         editMode: false,
+//         status: this.props.status
+//     }
     
-    activateEditMode = () => {
-        this.setState({
-            editMode: true
-        })
-    }
-    deactivateEditMode = (e) => {
-        this.setState({
-            editMode: false
-        })
+//     activateEditMode = () => {
+//         this.setState({
+//             editMode: true
+//         })
+//     }
+//     deactivateEditMode = (e) => {
+//         this.setState({
+//             editMode: false
+//         })
 
         
-        this.props.getStatus(this.state.status)
-    }
+//         this.props.getStatus(this.state.status)
+//     }
 
-    onStatusChange = (e) => {
-        this.setState({
-            status: e.currentTarget.value
-        })
-    }
+//     onStatusChange = (e) => {
+//         this.setState({
+//             status: e.currentTarget.value
+//         })
+//     }
 
-    componentDidUpdate(prevProps, prevState) {
+//     componentDidUpdate(prevProps, prevState) {
         
-        if(prevProps.status !== this.props.status) {
-            this.setState({
-                status: this.props.status
-            })
-        }
+//         if(prevProps.status !== this.props.status) {
+//             this.setState({
+//                 status: this.props.status
+//             })
+//         }
+//     }
+
+//     render() {
+//         return <div>
+//             {!this.state.editMode && <p className={s.status} onDoubleClick={this.activateEditMode}>{this.props.status}</p>}
+//             {this.state.editMode && <input onChange={this.onStatusChange} className={s.statusInput} autoFocus={true} onBlur={this.deactivateEditMode} value={this.state.status} />}
+//         </div>;
+//     }
+// }
+
+const ProfileStatus = (props) => {
+    const [editMode, setEditMode] = useState(false)
+    const [status, setStatus] = useState(props.status)
+
+    const activateEditMode = () => {
+        setEditMode(true)
+    }
+    const deactivateEditMode = () => {
+        setEditMode(false)
+        props.getStatus(status)
     }
 
-    render() {
-        return <div>
-            {!this.state.editMode && <p className={s.status} onDoubleClick={this.activateEditMode}>{this.props.status}</p>}
-            {this.state.editMode && <input onChange={this.onStatusChange} className={s.statusInput} autoFocus={true} onBlur={this.deactivateEditMode} value={this.state.status} />}
+    const onStatusChange = (e) => {
+        setStatus(e.currentTarget.value)
+    }
+
+    useEffect(() => {
+        setStatus(props.status)
+    }, [props.status])
+
+    return <div>
+            {!editMode && <p className={s.status} onDoubleClick={activateEditMode}>{props.status}</p>}
+            {editMode && <input onChange={onStatusChange} className={s.statusInput} autoFocus={true} onBlur={deactivateEditMode} value={status} />}
         </div>;
-    }
 }
 
 export default ProfileStatus;
